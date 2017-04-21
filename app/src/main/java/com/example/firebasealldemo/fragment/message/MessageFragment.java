285ad934e91948a3c0da151cbc0880499068b81e
ake <7473276@qq.com>
2017-04-22 07:38:57 +0800

    测试实时数据库

--- app/src/main/java/com/example/firebasealldemo/fragment/message/MessageFragment.java
+++ app/src/main/java/com/example/firebasealldemo/fragment/message/MessageFragment.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 import com.example.firebasealldemo.R;
 import com.example.firebasealldemo.mvp.MVPBaseFragment;
+import com.example.firebasealldemo.utils.RealTimeDb;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import static android.content.ContentValues.TAG;
@@ -17,9 +18,9 @@ import static android.content.ContentValues.TAG;
  *  邮箱 784787081@qq.com
  */
 
-public class MessageFragment extends MVPBaseFragment<MessageContract.View, MessagePresenter> implements MessageContract.View {
+public class MessageFragment extends MVPBaseFragment<MessageContract.View, MessagePresenter> implements MessageContract.View, View.OnClickListener {
 
-    private Button btn;
+    private Button btn,test;
 
     @Override
     public int getLayoutResId() {
@@ -29,23 +30,33 @@ public class MessageFragment extends MVPBaseFragment<MessageContract.View, Messa
     @Override
     public void initView(View view) {
         btn = (Button) view.findViewById(R.id.button2);
+        test = (Button) view.findViewById(R.id.button3);
+        test.setOnClickListener(this);
+        btn.setOnClickListener(this);
     }
 
     @Override
     public void initListener() {
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String token = FirebaseInstanceId.getInstance().getToken();
-                Log.d(TAG, token);
-                Toast.makeText(context, token, Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 
     @Override
     public void initData() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                String token = FirebaseInstanceId.getInstance().getToken();
+                Log.d(TAG, token);
+                Toast.makeText(context, token, Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.button3:
+                RealTimeDb instance = RealTimeDb.getInstance(context);
+                instance.sendStingTodb("");
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/firebasealldemo/utils/RealTimeDb.java
+++ app/src/main/java/com/example/firebasealldemo/utils/RealTimeDb.java
@@ -1,11 +1,15 @@
 package com.example.firebasealldemo.utils;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.example.firebasealldemo.Constants;
 import com.example.firebasealldemo.bean.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by seeker on 2017/4/21.
@@ -39,5 +43,24 @@ public class RealTimeDb {
     public RealTimeDb savaUserInfo(String userid,User user) {
         dbRef.child(userid).setValue(user);
         return realTimeDb;
+    }
+
+    public void sendStingTodb(String content) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        dbRef = database.getReference(Constants.realTimeDb_refence).child(Constants.Users);
+        DatabaseReference message  = database.getReference("message");
+        message.setValue("hellow Firebase db");
+        message.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(ctx, ""+value, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
